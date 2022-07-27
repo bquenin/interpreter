d4aacefdadd0fd21311b1d3eee67e9e0715dc933
qube <bertrand.quenin@gmail.com>
2022-07-26 22:50:08 -0700

    improving subtitles

--- cmd/interpreter/configuration/configuration.go
+++ cmd/interpreter/configuration/configuration.go
@@ -2,11 +2,14 @@ package configuration
 
 import (
 	_ "embed"
+	"fmt"
+	"image/color"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
 
+	"github.com/bquenin/interpreter/internal/translate"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
@@ -24,11 +27,27 @@ type Translator struct {
 	AuthenticationKey string `mapstructure:"authentication-key"`
 }
 
+type Subs struct {
+	Font       Font       `mapstructure:"font"`
+	Background Background `mapstructure:"background"`
+}
+
+type Font struct {
+	Color string `mapstructure:"color"`
+	Size  int    `mapstructure:"size"`
+}
+
+type Background struct {
+	Color   string `mapstructure:"color"`
+	Opacity int    `mapstructure:"opacity"`
+}
+
 type Configuration struct {
 	WindowTitle         string     `mapstructure:"window-title"`
 	RefreshRate         string     `mapstructure:"refresh-rate"`
 	ConfidenceThreshold float32    `mapstructure:"confidence-threshold"`
 	Translator          Translator `mapstructure:"translator"`
+	Subs                Subs       `mapstructure:"subs"`
 	Debug               bool
 }
 
@@ -79,4 +98,50 @@ func (c *Configuration) GetRefreshRate() time.Duration {
 		log.Panic().Msgf("unable to parse refresh rate: %s. Please check your configuration.", c.RefreshRate)
 	}
 	return refreshRate
+}
+
+func (c *Configuration) GetTranslator() (translate.Translator, error) {
+	var translator translate.Translator
+	var err error
+	switch c.Translator.API {
+	case "google":
+		translator, err = translate.NewGoogle(c.Translator.To)
+	case "deepl":
+		translator, err = translate.NewDeepL(c.Translator.To, c.Translator.AuthenticationKey)
+	default:
+		log.Fatal().Msgf("unsupported translator api: %s", c.Translator.API)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return translator, nil
+}
+
+func parseColorString(s string) (color.RGBA, error) {
+	var c color.RGBA
+	if len(s) != 7 {
+		return c, fmt.Errorf("color string length must be 7 but is %d", len(s))
+	}
+	if _, err := fmt.Sscanf(s, "#%02x%02x%02x", &c.R, &c.G, &c.B); err != nil {
+		return c, fmt.Errorf("unable to parse color string %s", s)
+	}
+	return c, nil
+}
+
+func (f *Font) GetColor() (color.RGBA, error) {
+	color, err := parseColorString(f.Color)
+	if err != nil {
+		return color, fmt.Errorf("invalid `subs.font.color` value: %w", err)
+	}
+	color.A = uint8(0xFF)
+	return color, nil
+}
+
+func (b *Background) GetColor() (color.RGBA, error) {
+	color, err := parseColorString(b.Color)
+	if err != nil {
+		return color, fmt.Errorf("invalid `subs.background.color` value: %w", err)
+	}
+	color.A = uint8(b.Opacity)
+	return color, nil
 }
