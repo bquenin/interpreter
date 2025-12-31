af0e106f56a416967816d846b2ed7323623bf17f
Bertrand <bertrand.quenin@gmail.com>
2025-12-30 19:40:23 -0800

    Release v2.0.0

--- src/interpreter/__init__.py
+++ src/interpreter/__init__.py
@@ -5,8 +5,8 @@ MeikiOCR, translates using Sugoi V4, and displays subtitles in a
 transparent overlay.
 """
 
-__version__ = "0.1.0"
-__commit__ = "3a3fb84"  # Short commit hash, updated on release
+__version__ = "2.0.0"
+__commit__ = "165ec22"  # Short commit hash, updated on release
 
 # Public API
 from .__main__ import main, list_windows
